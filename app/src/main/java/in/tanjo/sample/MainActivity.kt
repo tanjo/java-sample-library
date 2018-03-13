c5e3bc8df083e69d06a37d7df0d5b656d6c2373f
tanjo <tanjo@users.noreply.github.com>
2018-03-13 16:06:34 +0900

    サンプルアプリ

--- app/src/main/java/in/tanjo/sample/MainActivity.kt
+++ app/src/main/java/in/tanjo/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import `in`.tanjo.modeling.extension.toJson
 import `in`.tanjo.sample.model.Hoge
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val hoge = Hoge.fromJson("{\"fuga\":\"寿司が食べたい\"")
+        val hoge = Hoge.fromJson("{\"fuga\":\"寿司が食べたい\"}")
 
         val textView = this.findViewById<TextView>(R.id.activity_main_textview)
 
         val hogeString = hoge?.let { hoge.toJson() } ?: "Not working"
 
-        textView.text = hogeString
+        val fugaString = hoge?.let { hoge.fuga } ?: "null"
+
+        val text = "${fugaString}\n${hogeString}"
+
+        textView.text = text
+
+        val hoge2 = Hoge("piyo")
+        Log.i("in.tanjo.sample", hoge2.toJson()) // {"fuga":"piyo"}
+
+        val hoge3 = Hoge.fromJson(null)
+        Log.i("in.tanjo.sample", hoge3?.toJson() ?: "hoge3 is null.") // hoge3 is null.
     }
 }
